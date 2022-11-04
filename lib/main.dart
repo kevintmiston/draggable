f982f749cc96691ff3a7c0a31ee4ffb42993e866
unknown <kmiston13@gmail.com>
2022-11-04 08:41:13 +0700

    flutter

---
+++ lib/main.dart
@@ -0,0 +1,128 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  Color color1 = Colors.red;
+  Color color2 = Colors.amber;
+  Color targetColor;
+  bool isAccepted = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("Latihan Draggable"),
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: <Widget>[
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: <Widget>[
+                Draggable<Color>(
+                  data: color1,
+                  feedback: SizedBox(
+                    width: 50,
+                    height: 50,
+                    child: Material(
+                      color: color1.withOpacity(0.7),
+                      shape: const StadiumBorder(),
+                      elevation: 3,
+                    ),
+                  ),
+                  childWhenDragging: const SizedBox(
+                    width: 50,
+                    height: 50,
+                    child: Material(
+                      color: Colors.grey,
+                      shape: StadiumBorder(),
+                      // elevation: 3,
+                    ),
+                  ),
+                  child: SizedBox(
+                    width: 50,
+                    height: 50,
+                    child: Material(
+                      color: color1,
+                      shape: const StadiumBorder(),
+                      elevation: 3,
+                    ),
+                  ),
+                ),
+                Draggable<Color>(
+                  data: color2,
+                  feedback: SizedBox(
+                    width: 50,
+                    height: 50,
+                    child: Material(
+                      color: color2.withOpacity(0.7),
+                      shape: const StadiumBorder(),
+                      elevation: 3,
+                    ),
+                  ),
+                  childWhenDragging: const SizedBox(
+                    width: 50,
+                    height: 50,
+                    child: Material(
+                      color: Colors.grey,
+                      shape: StadiumBorder(),
+                      // elevation: 3,
+                    ),
+                  ),
+                  child: SizedBox(
+                    width: 50,
+                    height: 50,
+                    child: Material(
+                      color: color2,
+                      shape: const StadiumBorder(),
+                      elevation: 3,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            DragTarget(
+              onWillAccept: (value) => true,
+              onAccept: (value) {
+                isAccepted = true;
+                targetColor = value;
+              },
+              builder: (context, candidated, rejected) {
+                return (isAccepted)
+                  ? SizedBox(
+                      width: 100,
+                      height: 100,
+                      child: Material(
+                        color: targetColor,
+                        shape: const StadiumBorder(),
+                        // elevation: 3,
+                      ),
+                    )
+                  : const SizedBox(
+                      width: 100,
+                      height: 100,
+                      child: Material(
+                        color: Colors.grey,
+                        shape: StadiumBorder(),
+                        // elevation: 3,
+                      ),
+                    );
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
